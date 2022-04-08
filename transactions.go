b7cee729f6de2d82e3d029b99566b44d39a01fa6
Ian Bishop <1296987+porjo@users.noreply.github.com>
2022-04-08 15:33:59 +1000

    Modify Go HTTP client user agent

--- transactions.go
+++ transactions.go
@@ -10,6 +10,8 @@ import (
 
 const timeLayout = "2006-01-02T15:04:05-0700"
 const exportTransactionsURL = "https://www.ing.com.au/api/ExportTransactions/Service/ExportTransactionsService.svc/json/ExportTransactions/ExportTransactions"
+
+// Make Go HTTP client user-agent match headless-shell user-agent
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
 
 type transactionRequest struct {
