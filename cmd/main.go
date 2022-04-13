a8fe4717cb277d75f81de77856af3941a3f76077
Ian Bishop <1296987+porjo@users.noreply.github.com>
2022-04-13 21:17:26 +1000

    Update cmd

--- cmd/main.go
+++ cmd/main.go
@@ -11,13 +11,29 @@ import (
 	"github.com/porjo/ingaugo"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var bank ingaugo.Bank
+
 func main() {
-	//	wsURL := flag.String("ws-url", "ws://localhost:9222", "WebSsocket URL")
+
+	accounts := make(arrayFlags, 0)
+
+	wsURL := flag.String("ws-url", "", "WebSsocket URL e.g. ws://localhost:9222")
 	clientNumber := flag.String("clientNumber", "", "Client number")
 	accessPin := flag.String("accessPin", "", "Access pin")
-	accountNumber := flag.String("accountNumber", "", "Account number")
+	flag.Var(&accounts, "accountNumber", "Account number")
 	days := flag.Int("days", 30, "Number of days of transactions")
-	outCSVFile := flag.String("output", "", "File to write CSV data to. If empty, output to console.")
+	outputDir := flag.String("outputDir", "", "Directory to write CSV files. Defaults to current directory")
 	flag.Parse()
 	if *clientNumber == "" {
 		fmt.Printf("-clientNumber is required\n\n")
@@ -27,12 +43,21 @@ func main() {
 	}
 	if *accessPin == "" {
 		// check environmenta
-		*accessPin = os.Getenv("ING_ACCESS_PIN")
+		*accessPin = os.Getenv("ACCESS_PIN")
 		if *accessPin == "" {
-			fmt.Printf("-accessPin parameter or ING_ACCESS_PIN environment variable is required\n\n")
+			fmt.Printf("-accessPin parameter or ACCESS_PIN environment variable is required\n\n")
 			fmt.Println("Flags:")
 			flag.PrintDefaults()
 			os.Exit(1)
+		}
+	}
+	if *outputDir != "" {
+		info, err := os.Stat(*outputDir)
+		if os.IsNotExist(err) {
+			log.Fatalf("Directory %s does not exist", *outputDir)
+		}
+		if !info.IsDir() {
+			log.Fatalf("%s is not a directory", *outputDir)
 		}
 	}
 
@@ -40,27 +65,41 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
-	//bank := ingaugo.NewBankWithWS(*wsURL)
-	bank := ingaugo.NewBank()
+	if *wsURL != "" {
+		bank = ingaugo.NewBankWithWS(*wsURL)
+	} else {
+		bank = ingaugo.NewBank()
+	}
 
+	fmt.Printf("Fetching auth token...\n")
 	token, err := bank.Login(ctx, *clientNumber, *accessPin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if *accountNumber != "" {
-		log.Printf("fetching transactions\n")
-		trans, err := bank.GetTransactionsDays(*days, *accountNumber, token)
+	for _, acct := range accounts {
+		err := GetTransactions(*days, acct, token, *outputDir)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		if *outCSVFile != "" {
-			if err := os.WriteFile(*outCSVFile, trans, 0664); err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Printf("%s\n", trans)
-		}
 	}
+}
+
+func GetTransactions(days int, accountNumber, token, outputDir string) error {
+	log.Printf("Fetching transactions for account %s\n", accountNumber)
+	trans, err := bank.GetTransactionsDays(days, accountNumber, token)
+	if err != nil {
+		return err
+	}
+
+	file := accountNumber + ".csv"
+	if outputDir != "" {
+		file = outputDir + "/" + file
+	}
+	log.Printf("Writing CSV file %s\n", file)
+	if err := os.WriteFile(file, trans, 0664); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/porjo/ingaugo
 go 1.16
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20220321060548-7bc2623472b3
+	github.com/chromedp/cdproto v0.0.0-20220408044303-8559a4e76b35
 	github.com/chromedp/chromedp v0.8.0
 	github.com/vitali-fedulov/images3 v1.0.12
 )
